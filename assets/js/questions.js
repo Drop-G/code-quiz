f74d03fc5dc314970a2573d707be69fc2255b36f
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-10-22 18:11:13 -0500

    question array

---
+++ assets/js/questions.js
@@ -0,0 +1,43 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+      title: "What does HTML stand for?",
+      choices: ["Hyper Text Markup Lingo", "Hyper Text Makeup Label", "Hyper Text Markup Language"],
+      answer: "Hyper Text Markup Language"
+    },
+  {
+      title: "What does CSS stand for?",
+      choices: ["Castle Stronghold Sanctuary","Cascading Style Sheet","Cringy Silent Suburbia"],
+      answer: "Cascading Style Sheet"
+    },
+  {
+      title: "What is an Example of a symanctic HTML element?",
+      choices: ["div", "nav", "img"],
+      answer: "nav"
+    },
+  {
+      title: "What is a CDN",
+      choices: ["Credit Dentist Network", "Content Delivery Network", "Crying Dad Nuptual"],
+      answer: "Content Delivery Network"
+    },
+  {
+      title: "Which of these would be used with an image object to boost SEO?",
+      choices: ["div", "nav", "alt = "],
+      answer: "alt = "
+    },
+  {
+      title: "what command can help find problems in the browswer?",
+      choices: ["console.log()", "search", "google"],
+      answer: "console.log()"
+    },
+  {
+      title: "Whcih symbold is asscociated with JQuery",
+      choices: ["?", "$", "%"],
+      answer: "$"
+    },
+  {
+      title: "which do you use to clone a repository from gitHub",
+      choices: ["git add", "git commit", "git clone"],
+      answer: "git clone"
+    }
+  ];
